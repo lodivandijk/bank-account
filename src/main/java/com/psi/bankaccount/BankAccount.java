a7fa139787f05f0a30ae7c2a5bde42094dc471be
lodivandijk <lodi.van.dijk@gmail.com>
2023-10-14 08:04:05 +0100

    bank account getter/setters added

--- src/main/java/com/psi/bankaccount/BankAccount.java
+++ src/main/java/com/psi/bankaccount/BankAccount.java
@@ -1,5 +1,65 @@
 package com.psi.bankaccount;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class BankAccount {
-    
+    private @Id @GeneratedValue Long id;
+    private String accountName;
+    private Long accountBalance;
+
+    BankAccount(){}
+
+    BankAccount(String accountName, Long accountBalance){
+        this.accountName = accountName;
+        this.accountBalance = accountBalance;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getAccountName(){
+        return this.accountName;
+    }
+
+    public Long getAccountBalance(){
+        return this.accountBalance;
+    }
+  
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setAccountName(String accountName){
+        this.accountName = accountName;
+    }
+
+    public void changeAccountBalance(Long amount){
+        this.accountBalance = accountBalance + amount;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+        return true;
+        if (!(o instanceof BankAccount))
+        return false;
+        BankAccount bankAccount = (BankAccount)o;
+        return Objects.equals(this.id, bankAccount.id)  && Objects.equals(this.accountName, bankAccount.accountName);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.id, this.accountName, this.accountBalance);
+    }
+
+    @Override
+    public String toString(){
+        return "BankAccount{id= "+this.id+", name= "+this.accountName+", balance= "+this.accountBalance+"}";
+    }
 }
